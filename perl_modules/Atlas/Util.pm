b2ccb02efc1eb80f4b37c031846faba1fec489fb
Jonathan Manning <jmanning@ebi.ac.uk>
2021-06-24 12:31:50 +0100

    Auto-initalise configs from defaults

--- perl_modules/Atlas/Util.pm
+++ perl_modules/Atlas/Util.pm
@@ -28,6 +28,7 @@ package Atlas::Util;
 use 5.10.0;
 
 use File::Basename;
+use File::Copy;
 
 use base 'Exporter';
 our @EXPORT_OK = qw(
@@ -42,37 +43,52 @@ my $logger = Log::Log4perl::get_logger;
 
 =over 2
 
-=item _build_supporting_files_path
+=item _build_module_supporting_files_path
 
-Find where the supporting_files folder is
+Return the path of the supporting_files dir from the repository
 
 =cut
+
+sub _build_module_supporting_files_path {
+    
+    # Deduce the path to the config file from the path to this module.
+    # This module's location on the filesystem.
+    my $thisModulePath = __FILE__;
+
+    # The directory this module occupies.
+    my $thisModuleDir = dirname( $thisModulePath );
+
+    # First split the directories we have.
+    my @directories = File::Spec->splitdir( $thisModuleDir );
+
+    # Get up to the atlasprod directory.
+    while( $directories[ -1 ] ne "atlasprod" ) {
+        pop @directories;
+
+        unless( @directories ) {
+            die "ERROR - Cannot find atlasprod directory in path to Atlas::Common. Please ensure this module is installed under atlasprod.\n";
+        }
+    }
+
+    # Stick the remaining directories back together, now pointing to atlasprod directory.
+    # Check that the supporting_files dir is in the dir now in $atlasprodDir.
+    $result = File::Spec->catfile( @directories, "supporting_files" );
+
+    return $result
+}
+
+
+=item _build_supporting_files_path
+
+Find where the supporting_files folder is, prioritising env var
+
+=cut
+
 sub _build_supporting_files_path {
     
     my $result = $ENV{'ATLAS_META_CONFIG'};
     if(! defined $result){
-        # Deduce the path to the config file from the path to this module.
-        # This module's location on the filesystem.
-        my $thisModulePath = __FILE__;
-
-        # The directory this module occupies.
-        my $thisModuleDir = dirname( $thisModulePath );
-
-        # First split the directories we have.
-        my @directories = File::Spec->splitdir( $thisModuleDir );
-
-        # Get up to the atlasprod directory.
-        while( $directories[ -1 ] ne "atlasprod" ) {
-            pop @directories;
-
-            unless( @directories ) {
-                die "ERROR - Cannot find atlasprod directory in path to Atlas::Common. Please ensure this module is installed under atlasprod.\n";
-            }
-        }
-
-        # Stick the remaining directories back together, now pointing to atlasprod directory.
-        # Check that the supporting_files dir is in the dir now in $atlasprodDir.
-        $result = File::Spec->catfile( @directories, "supporting_files" );
+        $result =  _build_module_supporting_files_path();
     }
 
     unless( -d $result ) {
@@ -95,8 +111,20 @@ sub get_supporting_file {
 		$supporting_files_dir,
 		$file_name
 	);
-    unless( -r $result ) {
-        die "ERROR - Cannot read supporting file: $result -- please check it exists and is readable by your user ID.\n";
+    if ( ! -r $result ){
+        my $module_supporting_file_dir = _build_module_supporting_files_path()
+        my $result_template = File::Spec->catfile(
+            $module_supporting_files_dir,
+            "${file_name}.default"
+        );
+
+        if ( -r $result_template ){
+            warn "$result not present, initialising from $result_template"
+            copy( "${result}.default", ${result});
+        }
+        else {
+            die "ERROR - Cannot read supporting file: $result or default $result_template -- please check it exists and is readable by your user ID.\n";
+        }
     }
     return $result;
 }
