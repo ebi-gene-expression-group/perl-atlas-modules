42d8650de4575c01463ced4a774a453500566b70
Jonathan Manning <jmanning@ebi.ac.uk>
2021-06-24 13:48:34 +0100

    Fix typo

--- perl_modules/Atlas/Util.pm
+++ perl_modules/Atlas/Util.pm
@@ -114,7 +114,7 @@ sub get_supporting_file {
     if ( ! -r $result ){
         my $module_supporting_file_dir = _build_module_supporting_files_path();
         my $result_template = File::Spec->catfile(
-            $module_supporting_files_dir,
+            $module_supporting_file_dir,
             "${file_name}.default"
         );
 
